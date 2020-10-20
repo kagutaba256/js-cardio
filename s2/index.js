5f8f2db9a6395dac0236f8733cf5fdd6de7fac11
Sam Little <kagutaba256@gmail.com>
2020-10-20 15:56:53 -0700

    using replace instead of match with new regex

--- s2/index.js
+++ s2/index.js
@@ -76,12 +76,7 @@ const flatten_array4 = (arrays) => {
 
 const anagram1 = (s1, s2) => {
   const prep_string = (str) => {
-    return str
-      .trim()
-      .toLowerCase()
-      .match(/[a-z0-9]+/g)
-      .join('')
-      .split('')
+    return str.trim().toLowerCase().replace(/[^\w]/g, '').split('')
   }
   let s1_a = prep_string(s1)
   let s2_a = prep_string(s2)
@@ -105,3 +100,5 @@ const anagram1 = (s1, s2) => {
 }
 
 console.log(anagram1('Dormitory', 'dirty room'))
+console.log(anagram1('elbow', 'below'))
+console.log(anagram1('boatload', 'spinachs'))
